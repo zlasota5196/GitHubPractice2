e9bf48137417898d24425b797c1e8502fb9f9804
Zuzanna Lasota <zlasota5196@stu.d214.org>
2022-04-06 14:02:09 -0500

    Initial Commit

---
+++ GitHubPractice2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubPractice2
+//
+//  Created by Zuzu Lasota  on 4/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
